cff6d92b0187a7a1dbf1e9525ca7ba466d274463
caio cutrim <caiocutrim.js@gmail.com>
2018-09-06 09:48:57 -0300

    Add new adn edit buttons for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,36 @@
 class PortfoliosController < ApplicationController
+	def index
+		@portfolio_items = Portfolio.all
+	end
+	def new
+		@portfolio_item = Portfolio.new
+	end
+	def create
+		@portfolio_item = Portfolio.new(portfolio_item_params)
+		respond_to do |format|
+			if @portfolio_item.save
+				format.html { redirect_to portfolios_path, notice: 'Your portfolio is alive' }
+			else
+				format.html { render :new }
+			end
+		end
+	end
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+	def update
+		@portfolio_item = Portfolio.find(params[:id])
+	
+		respond_to do |format|
+			if @portfolio_item.update(portfolio_item_params)
+				format.html { redirect_to(portfolios_path, notice: 'Portfolio was successfully updated.') }
+			else
+				format.html { render action: 'edit' }
+			end
+		end
+	end
+	private
+	def portfolio_item_params
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+	end
 end
