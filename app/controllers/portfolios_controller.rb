63099c92c051af23fe870bd503c0cea2f83dfafa
caio cutrim <caiocutrim.js@gmail.com>
2018-09-06 11:29:18 -0300

    Implemented destroy actin for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -29,6 +29,17 @@ class PortfoliosController < ApplicationController
 			end
 		end
 	end
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+	def destroy
+		@portfolio_item = Portfolio.find(params[:id])
+		@portfolio_item.destroy
+	
+		respond_to do |format|
+			format.html { redirect_to portfolios_path , notice: 'Record was removed'}
+		end
+	end
 	private
 	def portfolio_item_params
 		params.require(:portfolio).permit(:title, :subtitle, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :blogs
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-=======
   resources :portfolios
-  get 'pages/home'
-  get 'pages/contat'
-  get 'pages/about'
-  resources :blogs
->>>>>>> Add portifolio via resource generator
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
