10e727f880809ad735855e3e4e603da9e378968f
caio cutrim <caiocutrim.js@gmail.com>
2018-09-10 16:59:12 -0300

    Implemented custom scopes to portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,44 @@
 # frozen_string_literal: true
 
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
 10.times do |blog|
 	Blog.create!(
-
 		title: "My blog post #{blog}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
+		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		topic_id: Topic.last.id
 	)
 end
 puts	"10 blog posts created"
 5.times do |skill|
 	Skill.create!(
-
 		title: "Rails #{skill}",
-		percent_utilized: 15
-
+		percent_utilized: 15,
 	)
 end
 
 puts '5 skills created'
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
-
 		title: "Portfolio title #{portfolio_item}",
-		subtitle: "my great service",
+		subtitle: "Ruby on rails",
 		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		main_image:"https://via.placeholder.com/600x400",
 		thumb_image:"https://via.placeholder.com/350x200",
-
 	)
 end
-puts '9 portfolio items created'
+puts '8 portfolio items created'
+
+8.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "Angular",
+		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		main_image:"https://via.placeholder.com/600x400",
+		thumb_image:"https://via.placeholder.com/350x200",
+	)
+end
+puts '8 portfolio items created'
