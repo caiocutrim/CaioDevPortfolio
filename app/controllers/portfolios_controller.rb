d28d6b900f07cb0bdc2a84e00daf4c1b95155043
caio cutrim <caiocutrim.js@gmail.com>
2018-09-21 19:37:35 -0300

    Add technologies_attributes in the view layer

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,7 @@ class PortfoliosController < ApplicationController
 	end
 	def new
 		@portfolio_item = Portfolio.new
+		3.times { @portfolio_item.technologies.build }
 	end
 	def create
 		@portfolio_item = Portfolio.new(portfolio_item_params)
@@ -42,6 +43,6 @@ class PortfoliosController < ApplicationController
 	end
 	private
 	def portfolio_item_params
-		params.require(:portfolio).permit(:title, :subtitle, :body)
+		params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
 	end
 end
