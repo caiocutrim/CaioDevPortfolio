3e8929382db530c28fbe910827e04c9b66dcc30c
caio cutrim <caiocutrim.js@gmail.com>
2018-09-24 00:30:20 -0300

    Added guest_user for current_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,8 +6,10 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 		3.times { @portfolio_item.technologies.build }
 	end
+
 	def create
-		@portfolio_item = Portfolio.new(portfolio_item_params)
+		@portfolio_item = Portfolio.new(portfolio_params)
+
 		respond_to do |format|
 			if @portfolio_item.save
 				format.html { redirect_to portfolios_path, notice: 'Your portfolio is alive' }
@@ -16,14 +18,16 @@ class PortfoliosController < ApplicationController
 			end
 		end
 	end
+
 	def edit
 		@portfolio_item = Portfolio.find(params[:id])
 	end
+
 	def update
 		@portfolio_item = Portfolio.find(params[:id])
 	
 		respond_to do |format|
-			if @portfolio_item.update(portfolio_item_params)
+			if @portfolio_item.update(portfolio_params)
 				format.html { redirect_to(portfolios_path, notice: 'Portfolio was successfully updated.') }
 			else
 				format.html { render action: 'edit' }
@@ -41,8 +45,10 @@ class PortfoliosController < ApplicationController
 			format.html { redirect_to portfolios_path , notice: 'Record was removed'}
 		end
 	end
+	
 	private
-	def portfolio_item_params
-		params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
-	end
+
+		def portfolio_params
+			params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+		end
 end
