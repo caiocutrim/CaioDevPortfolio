79256a1fefbabc989e16ca1a335ed84f1260d623
caio cutrim <caiocutrim.js@gmail.com>
2018-09-25 21:12:13 -0300

    Implement authorization gem

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,8 @@
 class PortfoliosController < ApplicationController
 	before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  access all: [:show, :index] , 
+         user: {except: [:destroy, :new, :create, :update, :edit]}, 
+         site_admin: :all
 	layout 'portfolio'	
 	def index
 		@portfolio_items = Portfolio.all
