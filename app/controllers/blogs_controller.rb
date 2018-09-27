239335372604f2ddd9b527972cd2692325c8adc1
caio cutrim <caiocutrim.js@gmail.com>
2018-09-27 00:36:59 -0300

    Implement partials for blog, fontawesome and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,8 +7,8 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
-    @page_title = "My portfolio blog"
+    @blogs = Blog.page(params[:page]).per(5)
+    @page_title = "My Blog"
   end
 
   # GET /blogs/1
