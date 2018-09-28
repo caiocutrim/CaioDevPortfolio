3ab3fd40334436001b0ba94d84bdf1f634ccbe27
caio cutrim <caiocutrim.js@gmail.com>
2018-09-28 12:18:50 -0300

    Implement authorization rules for portfolio controller and view for drag and drop feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,7 @@ class PortfoliosController < ApplicationController
   layout 'portfolio'  
 	before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   access all: [:show, :index] , 
-         user: {except: [:destroy, :new, :create, :update, :edit]}, 
+         user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, 
          site_admin: :all
 
 	def index
