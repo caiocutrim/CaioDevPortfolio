d3b8fd5a4f92b33da42a43046a65ba9be2adda98
caio cutrim <caiocutrim.js@gmail.com>
2018-10-03 20:50:37 -0300

    Add new nav bar styles for blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
   def login_helper style
 		if current_user.is_a?(GuestUser) 
       (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
 		else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
@@ -45,11 +44,11 @@ module ApplicationHelper
       }
     ]
   end
-  def nav_helper style, tag_type
+  def nav_helper class1, class2, tag_type
     nav_links = '' 
     nav_items.each do |item|
-     nav_links << "<#{tag_type}>" \
-          "<a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>" \
+     nav_links << "<#{tag_type} class=#{class1}>" \
+          "<a href='#{item[:url]}' class='#{class2} #{active? item[:url]}'>" \
           "#{item[:title]}</a></#{tag_type}>"
       
     end
